50ab446044f494abd7ca684c7e1fb789e2c37136
pop4959 <pop4959@gmail.com>
2019-12-02 22:19:41 -0800

    Remove unused logger

--- bukkit/src/main/java/com/griefcraft/lwc/LWCPlugin.java
+++ bukkit/src/main/java/com/griefcraft/lwc/LWCPlugin.java
@@ -2,11 +2,7 @@ package com.griefcraft.lwc;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
-
 public class LWCPlugin extends JavaPlugin {
-    
-    private Logger logger = this.getLogger();
     
     @Override
     public void onEnable() {
--- sponge/src/main/java/com/griefcraft/lwc/LWCPlugin.java
+++ sponge/src/main/java/com/griefcraft/lwc/LWCPlugin.java
@@ -1,7 +1,5 @@
 package com.griefcraft.lwc;
 
-import com.google.inject.Inject;
-import org.slf4j.Logger;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStoppedServerEvent;
@@ -9,9 +7,6 @@ import org.spongepowered.api.plugin.Plugin;
 
 @Plugin(id = "lwc", name = "LWC", version = "1.0")
 public class LWCPlugin {
-
-    @Inject
-    private Logger logger;
 
     @Listener
     public void onServerStart(GameStartedServerEvent event) {
