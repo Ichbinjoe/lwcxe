6682ef142fc4b405b897d6d56c46035a2d7d36f9
pop4959 <pop4959@gmail.com>
2019-12-02 21:58:52 -0800

    Multiproject boilerplate

---
+++ bukkit/src/main/java/com/griefcraft/lwc/LWCPlugin.java
@@ -0,0 +1,21 @@
+package com.griefcraft.lwc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class LWCPlugin extends JavaPlugin {
+    
+    private Logger logger = this.getLogger();
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+    
+}
